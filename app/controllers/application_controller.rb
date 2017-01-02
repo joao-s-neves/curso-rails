8577f10d3007d1b925ba838edc3c620eb01abd68
João Neves <joneves14@gmail.com>
2017-01-02 12:29:25 +0000

    commit message

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
   def hello
-    render html: "¡Hola, mundo!"
-  end
-  def goodbye
-    render html: "goodbye, world!"
+    render html: "hello, world!"
   end
 end
